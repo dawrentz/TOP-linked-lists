3035c2a05a1e22c084e22c4c1d56bd6788966bd0
dawrentz <156712569+dawrentz@users.noreply.github.com>
2024-12-21 14:57:39 -0600

    DRY and polish. Add comments.

--- linkedListMod.js
+++ linkedListMod.js
@@ -1,6 +1,8 @@
-console.clear();
-
 // linkedListMod.js
+
+// ====================================== init ====================================== //
+
+console.clear();
 
 // ====================================== Major Functions ====================================== //
 
@@ -15,46 +17,44 @@ function Node(value = null, nextNode = null) {
 //Issue: project asks for functions with one parameter (ex: "at(index) returns the node at the given index")
 //  but without a way to pass the node into the function, recursion won't work?
 
-//Solution: pass node as the second parameter but set the default to this.listHead (this.head is taken by function method "head()"")
+//Solution: pass node as the second parameter with the default set to this.listHead as needed
 //  This allows to only "at(index)" to be called
 
-//Idea: could wrap at(index) around another function that passes the node parameter, but that seems redundant
+//Idea: could wrap at(index) around another function that passes the node parameter, but that seems redundant and dumb, but maybe that's a standard practice?
 
 //linked list maker
 export class LinkedList {
   //init list with null listHead
+  //"this.head" is taken by the head() method
   constructor() {
     this.listHead = null;
   }
 
-  //create new node and make last
+  //create new node and make last in list
   append(value) {
     const newNode = Node(value);
 
-    if (this.listHead === null) {
-      this.listHead = newNode;
-    } else {
-      this.tail().nextNode = newNode;
-    }
+    //if empty list, newNode becomes the head
+    if (this.listHead === null) this.listHead = newNode;
+    // else current tail now points to newNode (appended to list)
+    else this.tail().nextNode = newNode;
   }
 
   //create new node and make first
-  //needs loop through list and update all? no just point to the old listHead
   prepend(value) {
-    const newNode = Node(value);
-
-    if (this.listHead === null) {
-      //set nextNode to current listHead then update this new node to be the listHead
-      this.listHead = newNode;
-    } else {
-      newNode.nextNode = this.listHead;
-      this.listHead = newNode;
-    }
+    //newNode points to current listHead and update newNode to listHead (prepend to list)
+    //if empty list, new head just points to "null" like it should
+    const newNode = Node(value, this.listHead);
+    this.listHead = newNode;
   }
 
+  //returns number of nodes in linked list
   size(node = this.listHead) {
+    //base case: no list
     if (node === null) return 0;
+    //base case: if last node
     if (node.nextNode === null) return 1;
+    //else add 1 and run again
     return 1 + this.size(node.nextNode);
   }
 
@@ -63,45 +63,54 @@ export class LinkedList {
   }
 
   tail() {
-    return this.search(this.listHead, "nextNode", null);
+    //if empty list, return head (null)
+    if (this.listHead === null) return this.listHead;
+    //tail is the node at index of (size() - 1)
+    //could also search for which node points to null
+    return this.at(this.size() - 1);
   }
 
+  //first index is zero
   at(index, node = this.listHead) {
-    const maxIndex = this.size() - 1;
-
     //error handling
+    const maxIndex = this.size() - 1;
     if (index > maxIndex || index < 0 || !Number.isInteger(index)) {
       return `Must input number between 0 and ${maxIndex}`;
     }
     //base case
     if (index === 0) return node;
 
-    //starts at head and uses user index-input as the number of moves, landing on desired node
+    //search starts at head (default) and uses the user index-input as the number of moves to make, landing on desired node
     return this.at(index - 1, node.nextNode);
   }
 
+  //removes tail
   pop() {
-    const newTail = this.search(this.listHead, "nextNode", this.tail());
+    //newTail is the node at index of (size() - 2)
+    const newTail = this.at(this.size() - 2);
+    //tail is whatever points to null
     newTail.nextNode = null;
   }
 
-  contains(searchValue, node = this.listHead) {
-    if (node === null) return false;
-    if (node.value === searchValue) return true;
-    if (node.nextNode === null) return false;
-    return this.contains(searchValue, node.nextNode);
+  //only checks if value is in list
+  contains(searchValue) {
+    //DRY it up a bit
+    if (this.find(searchValue) === "Value not found") return false;
+    else return true;
   }
 
+  //return index of value if found
   find(searchValue, index = 0) {
+    //base case: reach the end of list
     const maxIndex = this.size() - 1;
-
-    //error handling
     if (index > maxIndex) return "Value not found";
-
+    //base case: found it
     if (this.at(index).value === searchValue) return index;
+    //else search again
     return this.find(searchValue, ++index);
   }
 
+  //list out all formatted values
   toString(node = this.listHead) {
     if (node.nextNode === null) {
       return `( ${node.value} ) -> null`;
@@ -109,6 +118,7 @@ export class LinkedList {
     return `( ${node.value} )`.concat(` -> ${this.toString(node.nextNode)}`);
   }
 
+  //node at index is shifted right, update leftNode pointer and newNode pointer
   insertAt(value, index) {
     const newNode = Node(value);
     const leftNode = this.at(index - 1);
@@ -118,17 +128,12 @@ export class LinkedList {
     newNode.nextNode = newRightNode;
   }
 
+  //node at index is unlinked, update leftNode pointer
   removeAt(index) {
     const leftNode = this.at(index - 1);
     const rightNode = this.at(index + 1);
 
     leftNode.nextNode = rightNode;
-  }
-
-  //finds and returns node
-  search(node = this.listHead, searchProp, searchVal) {
-    if (node[searchProp] === searchVal) return node;
-    return this.search(node.nextNode, searchProp, searchVal);
   }
 }
 
@@ -141,7 +146,7 @@ list.append("parrot");
 list.append("hamster");
 list.append("snake");
 list.append("turtle");
-list.prepend("prependNode");
+list.prepend("prependNode"); //prepend to top
 
 console.log("===================== head =====================");
 console.log(list.head());
